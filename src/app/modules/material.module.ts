99773e78f66abcd9b79f0a016de5fdbe832b0319
Daniel Netzer <daniel@peak.studio>
2017-11-04 19:56:35 +0200

    Mid work on top toolbar

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -10,7 +10,8 @@ import {
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatInputModule
 } from '@angular/material';
 
 @NgModule({
@@ -24,7 +25,8 @@ import {
         MatToolbarModule,
         MatIconModule,
         MatSidenavModule,
-        MatListModule
+        MatListModule,
+        MatInputModule
     ],
     exports: [
         MatButtonModule,
@@ -36,7 +38,8 @@ import {
         MatToolbarModule,
         MatIconModule,
         MatSidenavModule,
-        MatListModule
+        MatListModule,
+        MatInputModule
     ]
 })
 export class MaterialModule { }
