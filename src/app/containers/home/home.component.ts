19f3b830b96f236d891f217edb0c0a78e989e9c2
Daniel Netzer <daniel@peak.studio>
2017-11-08 10:18:05 +0200

    Added _pallete.scss, styles directory

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,59 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Inject, ViewChildren, QueryList } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { SortableComponent } from 'ng2-dnd';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChildren(SortableComponent) sortables: QueryList<SortableComponent>;
+  private clone: HTMLElement;
 
   // TODO: Create HomeCardModel
-  baseCards = [{
-    title: 'Mislaka',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  },
-  {
-    title: 'Registration',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  }, {
-    title: 'Invitations',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  }, {
-    title: 'Mislaka',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  }, {
-    title: 'Events',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  }, {
-    title: 'Partners',
-    desc: 'desc',
-    more: 'more',
-    expanded: true
-  }];
+  baseCards = [
+    {
+      title: 'Registration',
+      desc: 'desc',
+      more: 'dsadsadsadsad',
+      expanded: true
+    }, {
+      title: 'Invitations',
+      desc: 'desc',
+      more: 'monytkuykghjhgjhgre',
+      expanded: true
+    }, {
+      title: 'Mislaka',
+      desc: 'desc',
+      more: 'mweqwewqeqewqore',
+      expanded: true
+    }, {
+      title: 'Events',
+      desc: 'desc',
+      more: 'moiuyiyiyre',
+      expanded: true
+    }, {
+      title: 'Partners',
+      desc: 'desc',
+      more: 'morewqewqee',
+      expanded: true
+    }];
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private document) { }
 
   ngOnInit() {
   }
 
-  handleDragStart() {
+  ngAfterViewInit() {
+    const appRoot = this.document.getElementById('app-root');
+    this.sortables.forEach((ref) => {
+      ref._elem.addEventListener('dragstart', (event) => {
+        this.clone = <HTMLElement>event.srcElement.cloneNode(true);
+        // this.clone.classList.remove('dnd-sortable-drag');
+        this.clone.classList.add('dnd-drag-clone');
+        appRoot.appendChild(this.clone);
+        event.dataTransfer.setDragImage(this.clone, 0, 0);
+      }, false);
+
+      ref._elem.addEventListener('dragend', (event) => {
+        appRoot.removeChild(this.clone);
+      });
+    });
+  }
+
+  handleDragStart(event) {
     // Collapse all cards
     this.baseCards.forEach(card => {
       card.expanded = false;
     });
+
   }
 
-  handleDragEnd() {
+  handleDragEnd(event) {
     // Expend all cards
     this.baseCards.forEach(card => {
       card.expanded = true;
